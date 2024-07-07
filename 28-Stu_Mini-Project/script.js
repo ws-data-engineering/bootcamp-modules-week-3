efa144a3d3a7ce6ad9c23562a437094aa4932a7c
data_will <ws.data.engineering@gmail.com>
2024-07-06 20:55:07 -0500

    Updated rps game

--- 28-Stu_Mini-Project/script.js
+++ 28-Stu_Mini-Project/script.js
@@ -1,103 +1,118 @@
-const handSign = {
-    rock: 'R',
-    paper: 'P',
-    scissors: 'S'
+// Javascript example of Rock | Paper | Scissors 
+function mainGame() {
+    let startGame = welcome();
+    if (startGame === 'end') {
+        return;
+    };
+    score();
+    decision();
 };
 
-function rockPaperScissors() {
-    let gameResults = {
-        wins: 0,
-        losses: 0,
-        games_played: 0,
-        invalid_entry: 0
-    };
+let matchResults = {
+    wins: 0,
+    losses: 0,
+    ties: 0
+};
 
-    const choice = Object.values(handSign);
-   
-    const randomIndex = Math.floor(Math.random() * choice.length);
-
-    const virtualPlayer = choice[randomIndex];
-
-    let gameDecision = prompt(`Welcome to Rock Paper Scissors! 
-        Would you like to play? Enter: (Y, N)`);
-
-    while (gameDecision !== null) {
-        if (gameDecision === 'Y' || gameDecision === 'N') {
+function welcome() {
+    let startQuestion;
+    while (startQuestion !== null) {
+        startQuestion = prompt(`Welcome to Rock Paper Scissors! Would you like to play? Enter: (Y, N)`);
+        if ((startQuestion === 'N') || (startQuestion === null)) {
+            return 'end';
+        } else if (startQuestion === 'Y') {
             break;
-        }
-        gameResults['invalid_entry']++;
-        gameDecision = prompt(`Invalid entry. Please enter: (Y, N)`);
-    };
-
-    while (gameDecision !== null && gameDecision !== 'Y') {
-        if (gameDecision === 'N') {
-            finalDecision = confirm(`Are you sure you don't want to play`);
-        }
-        if (finalDecision) {
-            break;
-        };
-    };
-
-    while (gameDecision === 'Y') {  
-        let playerChoice = prompt(`Please enter either R (Rock), P (Paper) or S (Scissors):`);
-  
-        while (playerChoice !== null) {        
-            if (playerChoice === 'R') {
-               break;
-            } else if (playerChoice === 'P') {
-                break;
-            } else if (playerChoice === 'S') {
-                break
-            } else {
-                gameResults['invalid_entry']++;
-                playerChoice = prompt(`Invalid entry. Please enter either R, P or S.`)
-            };
-        };
-
-        while (playerChoice === virtualPlayer) {
-            playerChoice = prompt(
-                `The game was a draw! You selected ${playerChoice} and the virtual player chose ${virtualPlayer}. Please choose again!`
-            );
-            while (playerChoice !== null) {
-                if (playerChoice === 'R' || playerChoice === 'P' || playerChoice === 'S') {
-                    break;
-                }
-                gameResults['invalid_entry']++;
-                playerChoice = prompt(`Invalid entry. Please enter: (R, P, S)`);
-            };
-        };
-
-        if (playerChoice === null) {
-            break;
-        } else if ((playerChoice === 'R' && virtualPlayer !== 'P') || 
-            (playerChoice === 'S' && virtualPlayer !== 'R') ||
-            (playerChoice === 'P' && virtualPlayer !== 'S')) {
-            alert(`You win! Your choice: ${playerChoice}. The virtual player: ${virtualPlayer}`);
-            gameResults['wins']++;
         } else {
-            alert(`You lose! Your choice: ${playerChoice}. The virtual player: ${virtualPlayer}`);
-            gameResults['losses']++;
+            alert(`Invalid entry. Please try again.`)
         };
-
-        let keepPlaying = prompt(`Would you like another game? Enter: (Y, N)`);
-
-        while (keepPlaying !== null) {
-            if (keepPlaying === 'Y' || keepPlaying === 'N') {
-                gameResults['games_played']++;
-                break;
-            };
-            gameResults['invalid_entry']++;
-            keepPlaying = prompt(`Invalid entry. Would you like another game? Enter: (Y, N)`);
-        };
-
-        if (keepPlaying === null) {
-            break;
-        } else if (keepPlaying === 'N') {
-            alert(`You have ${gameResults['wins']} wins & ${gameResults['losses']} losses. You played a total of ${gameResults['games_played']} games played. You had ${gameResults['invalid_entry']} invalid entries.`);
-            break;
-        };           
     };
 };
 
-rockPaperScissors();
+function game() {
+    let gameQuestion;
+    while (gameQuestion !== null) {
+        gameQuestion = prompt(`Please enter either R (Rock), P (Paper) or S (Scissors):`);
+        if ((gameQuestion === 'R') || (gameQuestion === 'P') || (gameQuestion === 'S')) {
+            break;
+        } else if (gameQuestion === null) {
+            return results();
+        } else {
+            alert(`Invalid entry. Please try again.`)
+        };
+    };
+    return gameQuestion;
+};
 
+function virtual() {
+    const handChoice = ['R', 'P', 'S'];
+    const randomIndex = Math.floor(Math.random() * handChoice.length);
+    const virtualChoice = handChoice[randomIndex];
+    console.log(virtualChoice);
+    return virtualChoice;
+};
+
+function score() {
+    let playerHand; 
+    while (playerHand !== null) {
+        playerHand = game();
+        virtualPlayer = virtual(); 
+        if (playerHand === virtualPlayer) {
+            alert(`It's a draw. Try again.`)
+            counter('tie');
+        } else if ((playerHand === 'R' && virtualPlayer !== 'P') || (playerHand === 'S' && virtualPlayer !== 'R')
+            || (playerHand === 'P' && virtualPlayer !== 'S')) {
+            alert(`You win.`);
+            counter('win');
+            break;
+        } else if (playerHand === undefined) {
+            break;
+        } else {
+            alert(`You lost.`);
+            counter('lost');
+            break;
+        };
+    };
+    return playerHand;
+};
+
+function decision() {
+    let playerDecision;
+    while (playerDecision !== null) {
+        let gameOptOut = '';
+        playerDecision = prompt(`Would you like another game? Enter: (Y, N)`);
+        if ((playerDecision === 'N') || (playerDecision === null)) {
+            break;
+        } else if ((playerDecision !== 'Y')) {
+            alert(`Invalid entry. Please try again.`)
+        } else {
+            gameOptOut = score();
+        };
+        if (gameOptOut === undefined) {
+            return;
+        }
+    };
+    return results();
+};
+
+function counter(result) {
+    switch (result) {
+        case 'win':
+            matchResults['wins']++;
+            break;
+        case 'lost':
+            matchResults['losses']++;
+            break;
+        case 'tie':
+            matchResults['ties']++;
+        default:
+            break;
+    };
+    return;
+};
+
+function results() {
+    let matchStats = matchResults;
+    alert(`Here are your match results. Wins: ${matchStats['wins']}. Losses: ${matchStats['losses']}. Ties: ${matchStats['ties']}. Thank you for playing!`);
+};
+
+mainGame();
