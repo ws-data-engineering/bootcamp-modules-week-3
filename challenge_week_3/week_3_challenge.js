1ca8383598b728b5ed2ce94e671bf8198445d4ce
data_will <ws.data.engineering@gmail.com>
2024-07-09 19:19:13 -0500

    added week 3 challenge code

--- challenge_week_3/week_3_challenge.js
+++ challenge_week_3/week_3_challenge.js
@@ -9,36 +9,25 @@ const collectEmployees = function() {
     while (true) {
       employeeObject = {};
 
-      employeeObject.firstName = prompt('Enter first name:');
-      if (employeeObject.firstName === null || employeeObject.firstName === '') {
-        return;
-      };
-
-      employeeObject.lastName = prompt('Enter last name:');
-      if (employeeObject.lastName === null || employeeObject.lastName === '') {
-        return;
-      };
-
-      employeeObject.salary = parseInt(prompt('Enter salary:'));
-      if (employeeObject.salary === null || employeeObject.salary === '') {
-        return;
-      } else if (isNaN(employeeObject.salary)) {
+      let firstName = prompt('Enter first name:');
+      let lastName = prompt('Enter last name:');
+      let salary = prompt('Enter salary:');
+      if (isNaN(salary)) {
         employeeObject.salary = 0;
       };
 
+      employeeObject.firstName = firstName;
+      employeeObject.lastName = lastName;
+      employeeObject.salary = salary;
+
       employeesArray.push(employeeObject);
 
-      employeeObject.addEmployee = prompt('Do you want to add another employee?');
-      if (employeeObject.addEmployee === null || employeeObject.addEmployee === '') {
-        return;
+      addEmployee = confirm('Do you want to add another employee?');
+      if (!addEmployee) {
+        break;
       };
-      if (employeeObject.addEmployee === 'No') {
-        return employeesArray;
-      };
-      if (employeeObject.addEmployee === 'Yes') {
-        continue;
-      };
-    };  
+    }; 
+    return employeesArray; 
 }
 
 // Display the average salary
@@ -47,23 +36,60 @@ const displayAverageSalary = function(employeesArray) {
   let numEmployee = employeesArray.length;
   let salaryTotal = 0;
   let salaryAverage;
+  let salary;
+  let conversionIndicator;
+  let decimal;
+  let mainInteger;
 
   for (let i = 0; i < employeesArray.length; i++) {
-    salaryTotal += employeesArray[i]['salary'];
+    salary = employeesArray[i]['salary'];
+    salary = String(salary);
+    if (salary.indexOf('.') !== -1) {
+      conversionIndicator = true;
+    } else {
+      conversionIndicator = false;
+    };    
   };
 
-  salaryAverage = salaryTotal / numEmployee;
+  if (conversionIndicator) {
+    for (let i = 0; i < employeesArray.length; i++) {
+      salary = employeesArray[i]['salary'];
+        decimal = parseFloat(salary);
+        salaryTotal += decimal;
+    };
+    salaryAverage = salaryTotal / numEmployee;
 
-  console.log(`The average salary between our (${numEmployee}) employee(s) is $${salaryAverage}`); 
+    let salaryString = String(salaryAverage);
+    let decimalIndex = salaryString.indexOf('.');
+    let slicePoint = salaryString.length - decimalIndex;
+    let final;
+
+    if (slicePoint > 2) {
+      salaryString = salaryString.slice(0, decimalIndex + 3);
+      final = parseFloat(salaryString);
+      final = Math.round(final);
+    }; 
+    final = parseFloat(salaryString);
+    f = final.toLocaleString("en-US", { maximumFractionDigits: 2, minimumFractionDigits: 2});
+    console.log(`The average salary between our ${numEmployee} employee(s) is $${f}`); 
+    } else {
+      for (let i = 0; i < employeesArray.length; i++) {
+        salary = employeesArray[i]['salary'];
+        mainInteger = parseFloat(salary);
+        salaryTotal += mainInteger;
+    };
+    salaryAverage = salaryTotal / numEmployee;
+    let final = salaryAverage.toLocaleString("en-US", { maximumFractionDigits: 2, minimumFractionDigits: 2});
+    console.log(`The average salary between our ${numEmployee} employee(s) is ${final}`); 
+  };
 }
 
 // Select a random employee
 const getRandomEmployee = function(employeesArray) {
   // TODO: Select and display a random employee
-
-  const randomIndex = Math.floor(Math.random() * employeesArray.length);
-  const firstName = employeesArray[randomIndex]['firstName'];
-  const lastName = employeesArray[randomIndex]['lastName'];
+  let randomIndex = Math.floor(Math.random() * employeesArray.length);
+  let firstName = employeesArray[randomIndex]['firstName'];
+  let lastName = employeesArray[randomIndex]['lastName'];
 
   console.log(`Congratulations to ${firstName} ${lastName}, our random drawing winner!`);
 }
